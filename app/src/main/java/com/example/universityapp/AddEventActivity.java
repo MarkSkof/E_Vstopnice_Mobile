801d646fd68cb7eb542b8052d4f38fbd5109d93b
mark <mark.skof5@gmail.com>
2024-01-13 23:36:57 +0100

    initial commit

---
+++ app/src/main/java/com/example/universityapp/AddEventActivity.java
@@ -0,0 +1,124 @@
+package com.example.universityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.util.Log;
+import android.view.View;
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddEventActivity extends AppCompatActivity {
+
+    private TextView status;
+    private EditText name;
+    private EditText date;
+    private EditText time;
+    private EditText spots;
+    private EditText place;
+
+    private RequestQueue requestQueue;
+    private String url = "https://is-eventim.azurewebsites.net/api/v1/event";
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_student);
+
+        name = (EditText) findViewById(R.id.new_event_name);
+        date = (EditText) findViewById(R.id.new_event_date);
+        time = (EditText) findViewById(R.id.new_event_time);
+        spots = (EditText) findViewById(R.id.new_event_spots);
+        place = (EditText) findViewById(R.id.new_event_place);
+        status = (TextView) findViewById(R.id.status);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+    }
+
+    public void addStudent(View view){
+        try {
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("name", name.getText());
+            jsonBody.put("time", date.getText() + "T" + time.getText());
+            jsonBody.put("numberOfSpots", Integer.parseInt(spots.getText().toString()));
+            jsonBody.put("placeId", Integer.parseInt(place.getText().toString()));
+
+            final String mRequestBody = jsonBody.toString();
+
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("LOG_VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("LOG_VOLLEY", error.toString());
+                }
+            }
+            ) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                        return null;
+                    }
+                }
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "Something went wrong";
+                    if (response != null) {
+                        if (String.valueOf(response.statusCode).equals("201")) {
+                            responseString = "Dogodek uspešno dodan";
+                        }
+                        status.setText(responseString);
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+
+                @Override
+                public Map<String,String> getHeaders() throws AuthFailureError
+                {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("ApiKey", "MarkZan123$");
+                    params.put("Content-Type", "application/json");
+                    return params;
+                }
+
+            };
+
+            requestQueue.add(stringRequest);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
